9003c06026de586e511f73af7b3c25b7e60858de
Joseph Allen <joe@jumprope.design>
2018-11-24 17:37:15 -0500

    final: touch ups to img-pop, adding container to control width, position, etc.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# templateNumber10
+# Image Pop:
+
+A web component that can be use on any html page by including the script and element
+
+script: `./img-pop.min.js`
+element: `<img-pop src='img.jpg' title='' text=''></img-pop>`
+
+> Images default to 100% width. Use an parent element around the web component to control position, width, etc.
+
+## To Update:
+
+Clone, npm install, update code, compile with `npm run build -- --target wc --name img-pop ./src/Import.vue`, then replace generated _.js_ files
+
+## Compatiblity:
+Edge 15 and above.
+
+Edge 15 and IE 11 and below do not have image animation, but will still display image (keep in mind, if you're targeting these browsers what you add to the `title` and `text` attributes should be considered since they will not be shown).
+
+---
+
+# Learned:
+- Building a universal web component with vue.
+
+# Struggled:
+
+# ToDo:
+Try to remove #app div
--- js/img-pop.js
+++ js/img-pop.js
@@ -692,12 +692,12 @@ var staticRenderFns = []
 
 // CONCATENATED MODULE: ./src/Import.vue?vue&type=template&id=7ea16102&shadow
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"545da000-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/HelloWorld.vue?vue&type=template&id=bc13d2a2&
-var HelloWorldvue_type_template_id_bc13d2a2_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"img-pop-container"},[_c('div',{ref:"injectStyle"}),_c('div',{staticClass:"img-pop-container-relative"},[_c('img',{ref:"isInitialImg",staticClass:"img",class:[{invisible : _vm.imageOpen}, 'img-pop', 'image_center', 'sept30'],attrs:{"width":"100%","src":_vm.src,"alt":_vm.alt},on:{"click":_vm.open}})]),_c('div',{staticClass:"img-pop-container-popped",class:{hide : !_vm.imageOpen}},[_c('div',{staticClass:"opaqueOverlay"}),_c('div',{staticClass:"close",on:{"click":_vm.close}},[_c('span',{staticClass:"close_bar"},[_vm._v(" ")]),_c('span',{staticClass:"close_bar"},[_vm._v(" ")])]),_c('div',{staticClass:"web-component"},[_c('div',{staticClass:"web-component-img"},[_c('img',{staticClass:"img isPoppedImg",attrs:{"src":_vm.src,"alt":_vm.alt}})]),(_vm.title || _vm.text)?_c('div',{staticClass:"web-component-text"},[_c('h3',[_vm._v(_vm._s(_vm.title))]),_c('p',[_vm._v(_vm._s(_vm.text))])]):_vm._e()])])])}
-var HelloWorldvue_type_template_id_bc13d2a2_staticRenderFns = []
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"545da000-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/HelloWorld.vue?vue&type=template&id=5b3f0c9a&
+var HelloWorldvue_type_template_id_5b3f0c9a_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"img-pop-container"},[_c('div',{ref:"injectStyle"}),_c('div',{staticClass:"img-pop-container-relative"},[_c('img',{ref:"isInitialImg",staticClass:"img",class:{invisible : _vm.imageOpen},attrs:{"width":"100%","src":_vm.src,"alt":_vm.alt},on:{"click":_vm.open}})]),_c('div',{staticClass:"img-pop-container-popped",class:{hide : !_vm.imageOpen}},[_c('div',{staticClass:"opaqueOverlay"}),_c('div',{staticClass:"close",on:{"click":_vm.close}},[_c('span',{staticClass:"close_bar"},[_vm._v(" ")]),_c('span',{staticClass:"close_bar"},[_vm._v(" ")])]),_c('div',{staticClass:"web-component"},[_c('div',{staticClass:"web-component-img"},[_c('img',{staticClass:"img isPoppedImg",attrs:{"src":_vm.src,"alt":_vm.alt}})]),(_vm.title || _vm.text)?_c('div',{staticClass:"web-component-text"},[_c('h3',[_vm._v(_vm._s(_vm.title))]),_c('p',[_vm._v(_vm._s(_vm.text))])]):_vm._e()])])])}
+var HelloWorldvue_type_template_id_5b3f0c9a_staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/HelloWorld.vue?vue&type=template&id=bc13d2a2&
+// CONCATENATED MODULE: ./src/components/HelloWorld.vue?vue&type=template&id=5b3f0c9a&
 
 // EXTERNAL MODULE: ./node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js
 var webcomponents_bundle = __webpack_require__("ec2e");
@@ -757,14 +757,6 @@ var webcomponents_bundle = __webpack_require__("ec2e");
     let component = this;
 
     window.addEventListener('resize', () => { component.close() });
-
-
-    setTimeout(function() {
-      console.log(component.$refs['isInitialImg'].style);
-    }, 1000);
-    // used to get the original img size and apply to shadow dom version,
-    // but is currently not responsive. Think of other way to implement.
-    // component.$refs['isInitialImg'].style.width = component.$refs['isInitialImg'].offsetWidth;
   },
   methods: {
 
@@ -904,8 +896,8 @@ if (style0.__inject__) style0.__inject__(context)
 
 var component = normalizeComponent(
   components_HelloWorldvue_type_script_lang_js_,
-  HelloWorldvue_type_template_id_bc13d2a2_render,
-  HelloWorldvue_type_template_id_bc13d2a2_staticRenderFns,
+  HelloWorldvue_type_template_id_5b3f0c9a_render,
+  HelloWorldvue_type_template_id_5b3f0c9a_staticRenderFns,
   false,
   injectStyles,
   null,
